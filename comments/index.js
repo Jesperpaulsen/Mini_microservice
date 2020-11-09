d3eee52fd9987eded0b6d65adc4db1e10dd0b6c7
Jesper Paulsen <jesper@jesper.no>
2020-11-09 22:01:38 +0100

    feat: created moderation service

--- comments/index.js
+++ comments/index.js
@@ -21,7 +21,7 @@ app.post('/posts/:id/comments', async (req, res) => {
 
   const comments = commentsByPostId[postId] || [];
 
-  const comment = { id: commentId, content }
+  const comment = { id: commentId, content, status: 'pending' }
 
   comments.push(comment);
 
@@ -39,8 +39,29 @@ app.post('/posts/:id/comments', async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   console.log('Received Event', req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === 'CommentModerated') {
+    const { postId, id, status, content } = data;
+
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find(comment => comment.id === id);
+    comment.status = status;
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        postId,
+        content,
+        status,
+      }
+    });
+  }
   res.send({});
 });
 
