50cc17d8d5d4a0713e121edf9b13cba1ff8c9410
Jesper Paulsen <jesper@jesper.no>
2020-11-09 20:39:05 +0100

    feat: added event bus

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,18 +14,34 @@ app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const postId = req.params.id;
   const { content } = req.body;
 
   const comments = commentsByPostId[postId] || [];
 
-  comments.push({ id: commentId, content });
+  const comment = { id: commentId, content }
+
+  comments.push(comment);
 
   commentsByPostId[postId] = comments;
 
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      comment,
+      postId,
+    }
+
+  })
+
   res.status(201).send(comments);
+});
+
+app.post('/events', (req, res) => {
+  console.log('Received Event', req.body.type);
+  res.send({});
 });
 
 app.listen(4001, () => {
